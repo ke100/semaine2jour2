4a2b0690fe41ddbf02b4f84dfd97510b89bfc3fd
ke100 <ogkush@hotmail.fr>
2020-04-07 17:52:29 +0200

    ajout de la réponse à la question posée

--- exo_02.rb
+++ exo_02.rb
@@ -1,2 +1,4 @@
 puts "Bonjour, monde !"
 puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+#Nous pouvons constater que contrairement à "print", la méthode "puts" (abréviation de put string) affiche le contenu sur une nouvelle ligne.
