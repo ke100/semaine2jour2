ca4dbbc3d0e302730be32f26c85059327ef36b18
ke100 <ogkush@hotmail.fr>
2020-04-07 19:21:52 +0200

    exercice7_b ok

---
+++ exo_07_b.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+#la différence avec le 07_a est qu'il y à ">" en plus à l'endroit ou l'utilisateur est censé répondre à la question
