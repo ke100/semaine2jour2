6d945e8de75761b46d0c405243451542e745816b
ke100 <ogkush@hotmail.fr>
2020-04-07 19:18:25 +0200

    exercice7_a ok

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+#gets donne une ligne de texte, y compris un saut de ligne à la fin. Ceci est l'entrée utilisateur, gets renvoie cette ligne de texte sous forme de valeur de chaîne. L'appel de chomp sur cette valeur supprime le saut de ligne.
