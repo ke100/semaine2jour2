4c386e9327afd700ae0f6c0d4fa312c6b402fdaf
ke100 <ogkush@hotmail.fr>
2020-04-07 18:03:01 +0200

    exercice4 ok

---
+++ exo_04.rb
@@ -0,0 +1,8 @@
+puts "Salut, ça farte ?
+
+Quand on exécute le programme nous recevons un message
+ d'erreur (1: unterminated string meets end of file)
+ce qui signifie qu'à la ligne numéro 1 un string non
+terminé rencontre la fin de la ligne, pour la simple
+et bonne raison qu'il manque le guillemet de fermeture
+après le point d'interrogation.
