b529c4c87eb324209259a9685fdca857f742ba2d
Alexis CADD <62930592+ke100@users.noreply.github.com>
2020-04-07 19:26:26 +0200

    Update exo_07_a.rb

--- exo_07_a.rb
+++ exo_07_a.rb
@@ -2,4 +2,4 @@ puts "Bonjour, c'est quoi ton blase ?"
 user_name = gets.chomp
 puts user_name
 
-#gets donne une ligne de texte, y compris un saut de ligne à la fin. Ceci est l'entrée utilisateur, gets renvoie cette ligne de texte sous forme de valeur de chaîne. L'appel de chomp sur cette valeur supprime le saut de ligne.
+#gets donne une ligne de texte, y compris un saut de ligne à la fin. Ceci est l'entrée utilisateur, gets renvoie cette ligne de texte sous forme de valeur de chaîne. L'appel de chomp sur cette valeur supprime le saut de ligne. "gets.chomp" est une demande faite à l'utilisateur.
