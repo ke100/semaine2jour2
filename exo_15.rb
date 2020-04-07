462d50ab66ed1ec453e108d98e217333bc1e005f
ke100 <ogkush@hotmail.fr>
2020-04-07 22:38:26 +0200

    exercice15 ok

---
+++ exo_15.rb
@@ -0,0 +1,12 @@
+puts "Quelle est ton année de naissance ?"
+year_of_birth = Integer(gets.chomp)
+
+age = 2020 - year_of_birth
+puts year_of_birth
+
+i= -1
+age.times do
+    puts age = i = i+1
+    e=year_of_birth
+puts year_of_birth = e+1
+end
