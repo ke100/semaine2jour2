1f6e2e98fae2f6730c088025dab3a764bdb87468
Alexis CADD <62930592+ke100@users.noreply.github.com>
2020-04-08 00:03:21 +0200

    Update exo_19.rb

--- exo_19.rb
+++ exo_19.rb
@@ -2,10 +2,10 @@ listemail  =  []
 number = 1
 x = 1
 50.times do
-    email << "julien.doe.#{number}@email.com"
+    listemail << "julien.doe.#{number}@email.com"
     number +=1
 end
 25.times do
-    puts email[x]
+    puts listemail[x]
     x += 2
 end
